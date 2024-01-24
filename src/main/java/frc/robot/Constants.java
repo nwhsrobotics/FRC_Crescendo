11e042b88fcedc44e98588a8bdfee76528519ae3
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-01-24 08:59:48 -0500

    limelights and swerveposestimator

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,7 +7,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 
 //Most values are from SDS SwerveLib setup for MK4_L2
