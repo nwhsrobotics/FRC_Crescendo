fcb82bd5ef67f47358c3403f816e469b07b90a08
perpetualCreations <tchen0584@gmail.com>
2024-02-03 15:55:20 -0500

    feat: swappable controllers. Either the smartest or dumbest feature I've ever written.

---
+++ src/main/java/frc/robot/subsystems/oi/ControlManager.java
@@ -0,0 +1,233 @@
+package frc.robot.subsystems.oi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OIConstants;
+
+/**
+ * Handles multiple control interfaces at the driver station.
+ */
+public class ControlManager {
+    /**
+     * Outputs derived from control interfaces.
+     */
+    public static class Outputs {
+        public static double xSpeed = 0;
+        public static double ySpeed = 0;
+        public static double rotatingSpeed = 0;
+    }
+    
+    /**
+     * Commands that are to be bound to button presses made by the driver.
+     * 
+     * These should be overwritten during robot initialization,
+     * replacing the default placeholders.
+     */
+    public static class DriverButtonCommands {
+        public static Command navXResetCommand;
+        public static Command toggleFieldRelativeCommand;
+        public static Command toggleAutonavigationCommand;
+        public static Command autonavigateToAmp;
+        public static Command autonavigateToSource;
+        public static Command autonavigateToStage;
+    }
+
+    /**
+     * "SendableChooser" for selecting the active driver controller.
+     * 
+     * Please send this to the dashboard in "RobotContainer,"
+     * after registering all controllers.
+     */
+    public static SendableChooser<Integer> driverControllerChooser = new SendableChooser<>();
+
+    /**
+     * "SendableChooser" for selecting the active gunner controller.
+     *
+     * Please send this to the dashboard in "RobotContainer,"
+     * after registering all controllers.
+     */
+    public static SendableChooser<Integer> gunnerControllerChooser = new SendableChooser<>();
+
+    private static HashMap<Integer, Controller> registry = new HashMap<>();
+    private static int driverPort = -1;
+    private static int gunnerPort = -2;
+
+    /**
+     * Generates a "Trigger" for a command to be bound to a button (defined in "DriverButtonCommands" and "GunnerButtonCommands").
+     * 
+     * The command will not executed by the Trigger,
+     * unless the port of the driver/gunner controller it is associated with is currently selected to be active.
+     * 
+     * Internal use only.
+     * 
+     * @param hid - "GenericHID" of the controller.
+     * @param port - the port of associated controller.
+     * @param command - the command to be executed when the button is pressed.
+     * @param isCommandDriver - whether the command is for the driver or gunner.
+     */
+    private static void makeTriggerForButton(GenericHID hid, int port, int button, Command command, boolean isCommandDriver) {
+        if (button == -1) {  // -1 means it should not be bound.
+            return;
+        }
+
+        new Trigger(() -> hid.getRawButton(button)).onTrue(new InstantCommand(() -> {
+            if ((isCommandDriver && port != driverPort) || (!isCommandDriver && port != gunnerPort)) {
+                return;
+            }
+
+           command.schedule(); 
+        }));
+    }
+
+    /**
+     * Register a controller (which should already be associated with a particular port).
+     * 
+     * You should NEVER register multiple controllers to a single port.
+     * If a port is already occupied, a message will be printed in the console,
+     * assuming the WPILib framework doesn't immediately crash.
+     * 
+     * @param controller - controller being registered.
+     */
+    public static void registerController(Controller controller) {
+        if (ControlManager.registry.put(controller.getPort(), controller) != null) {
+            System.out.println("Achtung! Handler for OI overwrote controller registered for port " + Integer.toString(controller.getPort()) + ".");
+        }
+
+        GenericHID hid = controller.getGenericHID();
+        makeTriggerForButton(hid, controller.getPort(), controller.getNavXResetButton(), DriverButtonCommands.navXResetCommand, true);
+        makeTriggerForButton(hid, controller.getPort(), controller.getFieldRelativeButton(), DriverButtonCommands.toggleFieldRelativeCommand, true);
+        makeTriggerForButton(hid, controller.getPort(), controller.getAutonavigationButton(), DriverButtonCommands.toggleAutonavigationCommand, true);
+        makeTriggerForButton(hid, controller.getPort(), controller.getAutonavigateToAmpButton(), DriverButtonCommands.autonavigateToAmp, true);
+        makeTriggerForButton(hid, controller.getPort(), controller.getAutonavigateToSourceButton(), DriverButtonCommands.autonavigateToSource, true);
+        makeTriggerForButton(hid, controller.getPort(), controller.getAutonavigateToStageButton(), DriverButtonCommands.autonavigateToStage, true);
+
+        Logger.recordOutput("controller." + Integer.toString(controller.getPort()) + ".name", controller.getName());
+    }
+
+    /**
+     * Processes available controllers and sorts them into the "SendableChooser" objects for driver and gunner controllers.
+     */
+    public static void buildChoosers() {
+        ControlManager.driverControllerChooser = new SendableChooser<>();
+        ControlManager.gunnerControllerChooser = new SendableChooser<>();
+        
+        for (String option : ControlManager.getControllers(true)) {
+            ControlManager.driverControllerChooser.addOption(option, ControlManager.getControllerPortFromLabel(option));
+        }
+
+        for (String option : ControlManager.getControllers(false)) {
+            ControlManager.gunnerControllerChooser.addOption(option, ControlManager.getControllerPortFromLabel(option));
+        }
+    }
+
+    /**
+     * Get the current port actively being used for driver inputs.
+     * 
+     * @return - integer ID for port.
+     */
+    public static int getDriverPort() {
+        return ControlManager.driverPort;
+    }
+    
+    /**
+     * Set the current port actively being used for driver inputs.
+     * 
+     * This method will be non-fatally unsuccessful,
+     * if no controller is registered with the new port,
+     * or if the controller is already being used by the gunner.
+     * 
+     * @return - boolean for whether change was successful.
+     */
+    public static boolean setDriverPort(int port) {
+        if (ControlManager.registry.get(port) == null || port == ControlManager.gunnerPort) {
+            return false;
+        }
+
+        ControlManager.driverPort = port;
+
+        return true;
+    }
+
+    /**
+     * Get the current port actively being used for gunner inputs.
+     * 
+     * @return - integer ID for port.
+     */
+    public static int getGunnerPort() {
+        return ControlManager.gunnerPort;
+    }
+
+    /**
+     * Set the current port actively being used for gunner inputs.
+     * 
+     * This method will be non-fatally unsuccessful,
+     * if no controller is registered with the new port,
+     * or if the controller is already being used by the driver.
+     * 
+     * @return - boolean for whether change was successful.
+     */
+    public static boolean setGunnerPort(int port) {
+        if (ControlManager.registry.get(port) == null || port == ControlManager.driverPort) {
+            return false;
+        }
+
+        ControlManager.gunnerPort = port;
+
+        return true;
+    }
+
+    /**
+     * Get list of available controllers.
+     * 
+     * @param areDrivers - whether to filter the list for only driver-enabled or gunner-enabled controllers.
+     * @return - labels for controllers.
+     */
+    public static ArrayList<String> getControllers(boolean areDrivers) {
+        ArrayList<String> options = new ArrayList<>();
+
+        for (int port : ControlManager.registry.keySet()) {
+            Controller controller = ControlManager.registry.get(port);
+            if (areDrivers && controller.getIntendedUser() > 0 || !areDrivers && controller.getIntendedUser() < 0) {
+                continue;
+            }
+
+            options.add(Integer.toString(port) + " - " + controller.getName());
+        }
+
+        return options;
+    }
+
+    /**
+     * Fetches port number for controller, from its label, as produced by "getControllers()."
+     * 
+     * @param label - label for controller.
+     * @return - integer ID for port.
+     */
+    public static int getControllerPortFromLabel(String label) {
+        return Integer.valueOf(label.split(" - ")[0]);
+    }
+
+    /**
+     * Process inputs.
+     * 
+     * Should be called repeatedly in a periodic function.
+     */
+    public static void process() {
+        ControlManager.driverPort = ControlManager.driverControllerChooser.getSelected();
+        ControlManager.gunnerPort = ControlManager.gunnerControllerChooser.getSelected();
+
+        Controller driverController = ControlManager.registry.get(ControlManager.driverPort);
+        Controller gunnerController = ControlManager.registry.get(ControlManager.gunnerPort);
+
+        ControlManager.Outputs.xSpeed = driverController.getX() * OIConstants.kTeleDriveMaxSpeedMetersPerSecond;
+        ControlManager.Outputs.ySpeed = driverController.getY() * OIConstants.kTeleDriveMaxSpeedMetersPerSecond;
+        ControlManager.Outputs.rotatingSpeed = driverController.getZ() * OIConstants.kTeleDriveMaxAngularSpeedRadiansPerSecond;
+    }
+}
