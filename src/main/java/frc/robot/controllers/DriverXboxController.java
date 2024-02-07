05fb344c3b4ff81ead9c07bc07e2906be02dd9df
NewAmazingPVP <gamingofpat@gmail.com>
2024-02-07 18:01:21 -0500

    removing redundant conventions
    removing redundant conventions

--- src/main/java/frc/robot/controllers/DriverXboxController.java
+++ src/main/java/frc/robot/controllers/DriverXboxController.java
@@ -7,13 +7,13 @@ import frc.robot.subsystems.oi.Controller;
 
 public class DriverXboxController implements Controller {
     private static final int port = 0;
-    private XboxController xboxController;
+    private final XboxController xboxController;
 
     private static final double kXYDeadband = 0.05;
     private static final double kZDeadband = 0.3;
 
     public DriverXboxController() {
-        this.xboxController = new XboxController(DriverXboxController.port);
+        xboxController = new XboxController(DriverXboxController.port);
     }
 
     @Override
@@ -23,7 +23,7 @@ public class DriverXboxController implements Controller {
 
     @Override
     public GenericHID getGenericHID() {
-        return this.xboxController;
+        return xboxController;
     }
 
     @Override
@@ -33,34 +33,21 @@ public class DriverXboxController implements Controller {
 
     @Override
     public double getX() {
-        return Controller.calculateSpeedWithDeadband(-this.xboxController.getLeftY() *getSlowMode(), kXYDeadband);
+        return Controller.calculateSpeedWithDeadband(-xboxController.getLeftY(), kXYDeadband);
         //return xLimiter.calculate(Controller.calculateSpeedWithDeadband(-this.xboxController.getLeftY(), kXYDeadband));
     }   
     
     @Override
     public double getY() {
-        return Controller.calculateSpeedWithDeadband(-this.xboxController.getLeftX() *getSlowMode(), kXYDeadband);
+        return Controller.calculateSpeedWithDeadband(-xboxController.getLeftX(), kXYDeadband);
         //return yLimiter.calculate(Controller.calculateSpeedWithDeadband(-this.xboxController.getLeftX(), kXYDeadband));
     }
     
     @Override
     public double getZ() {
-        return Controller.calculateSpeedWithDeadband(this.xboxController.getRightX()* getSlowModeRotation(), kZDeadband);
+        return Controller.calculateSpeedWithDeadband(xboxController.getRightX(), kZDeadband);
     }
 
-    public double getSlowMode(){
-        if(xboxController.getLeftTriggerAxis() > 0.1 || xboxController.getRightTriggerAxis() > 0.1){
-            return 0.2;
-        }
-        return 1.0;
-    }
-
-        public double getSlowModeRotation(){
-        if(xboxController.getLeftTriggerAxis() > 0.1 || xboxController.getRightTriggerAxis() > 0.1){
-            return 0.5;
-        }
-        return 1.0;
-    }
     @Override
     public int getNavXResetButton() {
         return 8;
