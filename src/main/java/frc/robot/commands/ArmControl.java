1b0ffbe14e3c5c2f334166d73babdf3bcd427931
Tim <efendievtimothy@gmail.com>
2024-02-08 19:59:05 -0500

    arm code

---
+++ src/main/java/frc/robot/commands/ArmControl.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ArmSubsystem;
+
+
+
+
+public class ArmControl extends Command {
+  private ArmSubsystem arm;
+  private double setPoint;
+  /** Creates a new ArmControl. */
+  public ArmControl(ArmSubsystem arm, double setPoint) {
+    this.arm = arm;
+    this.setPoint = setPoint;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    
+
+
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
