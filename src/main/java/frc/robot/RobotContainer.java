11fd4087f43d96e496fdf4ad5e184d847a099172
GoldenApple265 <97805427+GoldenApple265@users.noreply.github.com>
2024-03-02 15:15:53 -0500

    controller buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -9,56 +11,70 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.Constants.FavoritePositions;
 import frc.robot.commands.SwerveJoystickDefaultCmd;
-import frc.robot.controllers.DriverJoysticksController;
-import frc.robot.controllers.DriverLeftJoysticksController;
-import frc.robot.controllers.DriverXboxController;
+import frc.robot.commands.WristIntakeCmd;
+import frc.robot.commands.IntakeCmd;
+import frc.robot.controllers.*;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.oi.ControlManager;
 
 public class RobotContainer {
     public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-    public static XboxController gunner = new XboxController(3);
     public final ArmSubsystem armSubsystem = new ArmSubsystem();
     public final WristSubsystem wristSubsystem = new WristSubsystem();
+
     //object for presenting selection of options in shuffleboard/ smartdashboard
     SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser("Auto Square");
     public final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
     public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
-    public final WristIntakeSubsystem wristInstakeSubsystem = new WristIntakeSubsystem();
+    public final WristIntakeSubsystem wristIntakeSubsystem = new WristIntakeSubsystem();
     public final ClimbSubsystem climbSubsystem = new ClimbSubsystem();
+
+    public final WristIntakeCmd wristIntakeCmdForward = new WristIntakeCmd(wristIntakeSubsystem, true);
+    public final WristIntakeCmd wristIntakeCmdBackward = new WristIntakeCmd(wristIntakeSubsystem, false);
+    public final IntakeCmd intakeCmdForward = new IntakeCmd(intakeSubsystem, true);
+    public final IntakeCmd intakeCmdBackward = new IntakeCmd(intakeSubsystem, false);
+
+    public static XboxController gunner = new XboxController(10);
+
+    public final JoystickButton gunner_A = new JoystickButton(gunner, 1); // Button A
+    public final JoystickButton gunner_B = new JoystickButton(gunner, 2); // Button B
+    public final JoystickButton gunner_X = new JoystickButton(gunner, 3); // Button X
+    public final JoystickButton gunner_Y = new JoystickButton(gunner,4); // Button Y
+    public final JoystickButton gunner_LB = new JoystickButton(gunner, 5); // Left bumper
+    public final JoystickButton gunner_RB = new JoystickButton(gunner, 6); // Right bumper
+    public final JoystickButton gunner_V = new JoystickButton(gunner, 7); // View button
+    public final JoystickButton gunner_M = new JoystickButton(gunner, 8); // Menu button
+    public final JoystickButton gunner_LS = new JoystickButton(gunner, 9); // Left Stick Button
+    public final JoystickButton gunner_RS = new JoystickButton(gunner, 10); // Right Stick Button
+
+    public final POVButton gunner_pov0 = new POVButton(gunner, 0);
+    public final POVButton gunner_pov90 = new POVButton(gunner, 90);
+    public final POVButton gunner_pov180 = new POVButton(gunner, 180);
+    public final POVButton gunner_pov270 = new POVButton(gunner, 270);
+
 
     public RobotContainer() {
         //command for setting arm to the amp position
-        InstantCommand armLockAmp = new InstantCommand(() -> {
-            armSubsystem.ampPreset();
-        });
+        InstantCommand armLockAmp = new InstantCommand(() -> {armSubsystem.ampPreset();});
         armLockAmp.addRequirements(armSubsystem);
         //command for setting arm to the source position
-        InstantCommand armLockSource = new InstantCommand(() -> {
-            armSubsystem.sourcePreset();
-        });
+        InstantCommand armLockSource = new InstantCommand(() -> {armSubsystem.sourcePreset();});
         armLockSource.addRequirements(armSubsystem);
         //command for setting wrist to the amp position
-        InstantCommand wristLockAmp = new InstantCommand(() -> {
-            wristSubsystem.ampPreset();
-        });
+        InstantCommand wristLockAmp = new InstantCommand(() -> {wristSubsystem.ampPreset();});
         wristLockAmp.addRequirements(wristSubsystem);
         //command for setting wrist to the source position
-        InstantCommand wristLockSource = new InstantCommand(() -> {
-            wristSubsystem.sourcePreset();
-        });
+        InstantCommand wristLockSource = new InstantCommand(() -> {wristSubsystem.sourcePreset();});
         wristLockSource.addRequirements(wristSubsystem);
         //command for letting the gunner freely adjust the arm position
-        InstantCommand armAdjust = new InstantCommand(() -> {
-            armSubsystem.adjustAngle(gunner.getLeftY());
-        });
+        InstantCommand armAdjust = new InstantCommand(() -> {armSubsystem.adjustAngle(gunner.getLeftY());});
         armAdjust.addRequirements(armSubsystem);
         //command for letting the gunner freely adjust the wrist position
-        InstantCommand wristAdjust = new InstantCommand(() -> {
-            wristSubsystem.adjustAngle(gunner.getRightY());
-        });
+        InstantCommand wristAdjust = new InstantCommand(() -> {wristSubsystem.adjustAngle(gunner.getRightY());});
         wristAdjust.addRequirements(wristSubsystem);
         //command for letting you adjust the wrist and arm together
         ParallelCommandGroup parallel = new ParallelCommandGroup(armAdjust, wristAdjust);
@@ -67,95 +83,55 @@ public class RobotContainer {
         //command group that had built in logic 
         SequentialCommandGroup seq = new SequentialCommandGroup();
         seq.addRequirements(wristSubsystem, armSubsystem);
-        if (gunner.getAButton()) {
+            if(gunner_V.getAsBoolean()){
 
-            seq.addCommands(
-                    armLockAmp,
-                    wristLockAmp
-            );
-        }
+                seq.addCommands(
+                  armLockAmp,
+                  wristLockAmp
+              );
+              }
+          
+            if(gunner_M.getAsBoolean()){
+                seq.addCommands(
+                  armLockSource,
+                  wristLockSource
+              );
+              }
 
-        if (gunner.getBButton()) {
-            seq.addCommands(
-                    armLockSource,
-                    wristLockSource
-            );
-        }
-
-        if (!gunner.getAButton() && !gunner.getBButton()) {
-            seq.addCommands(
-                    parallel
-            );
-        }
+            if(!gunner_V.getAsBoolean() && !gunner_M.getAsBoolean()){
+                seq.addCommands(
+                  parallel
+                );
+              }
 
 
         armSubsystem.setDefaultCommand(seq);
         wristSubsystem.setDefaultCommand(seq);
-
+        
         // Creates instant commands for the different robot functionalities
-        InstantCommand intakeOn = new InstantCommand(() -> {
-            intakeSubsystem.forwards();
-        });
-        InstantCommand intakeOff = new InstantCommand(() -> {
-            intakeSubsystem.deactivate();
-        });
+        InstantCommand climbUp = new InstantCommand(() -> {climbSubsystem.moveUp();});
+        InstantCommand climbDown = new InstantCommand(() -> {climbSubsystem.moveDown();});
 
-        InstantCommand wristIntakeOn = new InstantCommand(() -> {
-            wristInstakeSubsystem.forwards();
-        });
-        InstantCommand wristIntakeOff = new InstantCommand(() -> {
-            wristInstakeSubsystem.stop();
-        });
-
-        InstantCommand climbUp = new InstantCommand(() -> {
-            climbSubsystem.moveUp();
-        });
-        InstantCommand climbDown = new InstantCommand(() -> {
-            climbSubsystem.moveDown();
-        });
-
-        InstantCommand shoot = new InstantCommand(() -> {
-            shooterSubsystem.stepIndex();
-        });
-
-        // TODO: Make it so you only have to press one button for each preset
-        InstantCommand wristAmpPreset = new InstantCommand(() -> {
-            wristSubsystem.ampPreset();
-        });
-        InstantCommand wristSourcePreset = new InstantCommand(() -> {
-            wristSubsystem.sourcePreset();
-        });
-
-        InstantCommand armAmpPreset = new InstantCommand(() -> {
-            armSubsystem.ampPreset();
-        });
-        InstantCommand armSourcePreset = new InstantCommand(() -> {
-            armSubsystem.sourcePreset();
-        });
+        InstantCommand shoot = new InstantCommand(() -> {shooterSubsystem.stepIndex();});
 
         // Registers the instant commands
-        NamedCommands.registerCommand("intakeOn", intakeOn);
-        NamedCommands.registerCommand("intakeOff", intakeOff);
-
-        NamedCommands.registerCommand("wristIntakeOn", wristIntakeOn);
-        NamedCommands.registerCommand("wristIntakeOff", wristIntakeOff);
-
         NamedCommands.registerCommand("climbUp", climbUp);
         NamedCommands.registerCommand("climbDown", climbDown);
 
         NamedCommands.registerCommand("shoot", shoot);
 
-        NamedCommands.registerCommand("wristAmpPreset", wristAmpPreset);
-        NamedCommands.registerCommand("wristSourcePreset", wristSourcePreset);
-
-        NamedCommands.registerCommand("armAmpPreset", armAmpPreset);
-        NamedCommands.registerCommand("armSourcePreset", armSourcePreset);
+        // Binds commands to buttons
+        gunner_A.whileTrue(wristIntakeCmdForward);
+        gunner_B.whileTrue(wristIntakeCmdBackward);
+        gunner_X.whileTrue(intakeCmdForward);
+        gunner_X.whileTrue(intakeCmdBackward);
+        gunner_LB.onTrue(shoot);
+        gunner_pov90.onTrue(climbDown);
+        gunner_pov270.onTrue(climbUp);
 
         // initialize driver button commands.
         ControlManager.DriverButtonCommands.navXResetCommand = new InstantCommand(() -> swerveSubsystem.gyro.zeroYaw());
-        ControlManager.DriverButtonCommands.toggleFieldRelativeCommand = new InstantCommand(() -> {
-            swerveSubsystem.isFieldRelative = !swerveSubsystem.isFieldRelative;
-        });
+        ControlManager.DriverButtonCommands.toggleFieldRelativeCommand = new InstantCommand(() -> {swerveSubsystem.isFieldRelative = !swerveSubsystem.isFieldRelative;});
         ControlManager.DriverButtonCommands.toggleAutonavigationCommand = new InstantCommand(() -> swerveSubsystem.autonavigator.toggle());
         ControlManager.DriverButtonCommands.autonavigateToAmp = new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(FavoritePositions.AMP));
         ControlManager.DriverButtonCommands.autonavigateToSource = new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(FavoritePositions.SOURCE));
@@ -163,7 +139,7 @@ public class RobotContainer {
         ControlManager.DriverButtonCommands.autonavigateToTopStage = new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(FavoritePositions.TOPSTAGE));
         ControlManager.DriverButtonCommands.autonavigateToMidStage = new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(FavoritePositions.MIDSTAGE));
         ControlManager.DriverButtonCommands.autonavigateToBottomStage = new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(FavoritePositions.BOTTOMSTAGE));
-
+        
         // reserve gunner port.
         ControlManager.reserveController(3);  // THE GUNNER CONTROLLER SHOULD BE ON PORT 3.
 
@@ -192,6 +168,7 @@ public class RobotContainer {
 
         SmartDashboard.putData("Auto Chooser", autoChooser);
         swerveSubsystem.setDefaultCommand(new SwerveJoystickDefaultCmd(swerveSubsystem));
+
     }
 
     public Command getAutonomousCommand() {
