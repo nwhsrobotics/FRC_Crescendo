671bf6ee2edfac5fa7a4154482909b43e9e0c880
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-03-03 15:24:58 -0500

    fix the robot orientation while driving with swerve pos estimator
    fix the robot orientation while driving with swerve pos estimator

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -87,7 +87,7 @@ public class SwerveSubsystem extends SubsystemBase {
     public final AHRS gyro = new AHRS(SerialPort.Port.kUSB);
 
     //odometry is a system to keep track of robots current position and rotation on the fields based on the coordinate system
-    private final SwerveDrivePoseEstimator odometer = new SwerveDrivePoseEstimator(
+    public final SwerveDrivePoseEstimator odometer = new SwerveDrivePoseEstimator(
             DriveConstants.kDriveKinematics,
             Rotation2d.fromDegrees(getHeading()),
             getModulePositions(),
