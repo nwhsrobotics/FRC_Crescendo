ebc58f390a20279157c94c8900bd0b4cb7dc3c0a
NewAmazingPVP <gamingofpat@gmail.com>
2024-03-05 16:15:13 -0500

    Update ControlManager.java

--- src/main/java/frc/robot/subsystems/oi/ControlManager.java
+++ src/main/java/frc/robot/subsystems/oi/ControlManager.java
@@ -313,4 +313,21 @@ public class ControlManager {
         targetingForwardSpeed *= -1.0;
         return targetingForwardSpeed;
     }
+
+    private static double distanceFromLimelight(double targetOffsetAngle_Vertical){
+        // how many degrees back is your limelight rotated from perfectly vertical?
+        double limelightMountAngleDegrees = 25.0; 
+
+        // distance from the center of the Limelight lens to the floor
+        double limelightLensHeightInches = 20.0; 
+
+        // distance from the target to the floor
+        double goalHeightInches = 60.0; 
+
+        double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+        double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+        //calculate distance
+        return (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
+    }
 }
