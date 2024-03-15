fd85e44f20909c1c62edd069b1f8d471d7ce83fc
nwhsrobotics <jagbotics4638@gmail.com>
2024-03-14 21:17:53 -0400

    better paths
    better paths

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,6 +11,8 @@ public class ClimbSubsystem extends SubsystemBase {
     public final CANSparkMax rightClimbMotor = new CANSparkMax(Constants.CANAssignments.CLIMB_RIGHT_MOTOR_ID, MotorType.kBrushless);
 
     public ClimbSubsystem() {
+        leftClimbMotor.setIdleMode(IdleMode.kBrake);
+        rightClimbMotor.setIdleMode(IdleMode.kBrake);
         leftClimbMotor.setInverted(true);
     }
 
