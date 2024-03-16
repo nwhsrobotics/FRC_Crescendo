b3ee27b24fde340d86a21298aad9d0b71d776ea1
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-03-16 09:47:37 -0400

    arm

--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -2,9 +2,12 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.WristConstants;
@@ -14,11 +17,12 @@ public class WristSubsystem extends SubsystemBase {
     private final CANSparkMax wristMotor;
     private final SparkPIDController wristPidController;
     private final RelativeEncoder wristRelativeEncoder;
+    private AbsoluteEncoder wristAbsoluteEncoder;
     private final double currentPosition;
     private double desiredPosition;
     private final boolean autoLockEnabledAmp = false;
     private final boolean autoLockEnabledSource = false;
-
+    
 
     // Constructor for WristSubsystem
     public WristSubsystem() {
@@ -27,7 +31,8 @@ public class WristSubsystem extends SubsystemBase {
         wristRelativeEncoder = wristMotor.getEncoder();
         wristPidController = wristMotor.getPIDController();
         wristPidController.setP(Constants.WristConstants.WRIST_PID_P);
-        currentPosition = wristRelativeEncoder.getPosition();
+        currentPosition = wristAbsoluteEncoder.getPosition();
+        
 
     }
 
