c530475ab49bb0840ad17584b0a10dcf3cd60b91
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-03-24 19:10:32 -0400

    vision autonomous testing
    vision autonomous testing

---
+++ src/main/java/frc/robot/autos/AutoSelector.java
@@ -0,0 +1,18 @@
+package frc.robot.autos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.FavoritePositions;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class AutoSelector {
+    public AutoSelector(SwerveSubsystem swerve){
+        SendableChooser<Command> autoChooser = new SendableChooser<>();
+        autoChooser.addOption("B [5 NOTE]", new Auto(swerve, new ArrayList<>(List.of(FavoritePositions.FRONTLEFT, FavoritePositions.FRONTLEFTMOST, FavoritePositions.FRONTRIGHT, FavoritePositions.FRONTRIGHTMOST)), 4));
+        SmartDashboard.putData(autoChooser);
+    }
+}
