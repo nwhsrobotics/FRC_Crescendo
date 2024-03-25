1e4b42861dd1d109aa910394cb01d032202d7eae
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-03-25 17:36:52 -0400

    much better swerve slew rate implementation like most teams
    much better swerve slew rate implementation like most teams

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -13,6 +14,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -24,6 +26,8 @@ import frc.robot.Constants.CANAssignments;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.limelight.LimelightHelpers;
+import frc.robot.subsystems.oi.ControlManager;
+
 import org.littletonrobotics.junction.Logger;
 
 /**
@@ -94,6 +98,15 @@ public class SwerveSubsystem extends SubsystemBase {
     //The default standard deviations of the module states are 0.1 meters for x, 0.1 meters for y, and 0.1 radians for heading. 
     //The default standard deviations of the vision measurements are 0.9 meters for x, 0.9 meters for y, and 0.9 radians for heading.
     //Decrease standard deviations to trust the data more (rn the vision is mostly insignificant compared to module state)
+
+    // Slew rate filter variables for controlling lateral acceleration
+    private double currentRotation = 0.0;
+    private double currentTranslationDir = 0.0;
+    private double currentTranslationMag = 0.0;
+
+    private SlewRateLimiter magLimiter = new SlewRateLimiter(DriveConstants.kMagnitudeSlewRate);
+    private SlewRateLimiter rotLimiter = new SlewRateLimiter(DriveConstants.kRotationalSlewRate);
+    private double prevTime = WPIUtilJNI.now() * 1e-6;
 
     /**
      * Constructor for the SwerveSubsystem class.
@@ -443,6 +456,85 @@ public class SwerveSubsystem extends SubsystemBase {
         Logger.recordOutput("swerve.odometer.xCoordinate", odometer.getEstimatedPosition().getX());
         Logger.recordOutput("swerve.odometer.yCoordinate", odometer.getEstimatedPosition().getY());
         Logger.recordOutput("swerve.odometer.rotation", odometer.getEstimatedPosition().getRotation().getDegrees());
+    }
+
+    /**
+     * Method to drive the robot using joystick info.
+     *
+     * @param xSpeed        Speed of the robot in the x direction (forward).
+     * @param ySpeed        Speed of the robot in the y direction (sideways).
+     * @param rot           Angular rate of the robot.
+     * @param fieldRelative Whether the provided x and y speeds are relative to the
+     *                      field.
+     * @param rateLimit     Whether to enable rate limiting for smoother control.
+     */
+    public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative, boolean rateLimit) {
+        
+        double xSpeedCommanded;
+        double ySpeedCommanded;
+
+        if (rateLimit) {
+        // Convert XY to polar for rate limiting
+        double inputTranslationDir = Math.atan2(ySpeed, xSpeed);
+        double inputTranslationMag = Math.sqrt(Math.pow(xSpeed, 2) + Math.pow(ySpeed, 2));
+
+        // Calculate the direction slew rate based on an estimate of the lateral acceleration
+        double directionSlewRate;
+        if (currentTranslationMag != 0.0) {
+            directionSlewRate = Math.abs(DriveConstants.kDirectionSlewRate / currentTranslationMag);
+        } else {
+            directionSlewRate = 500.0; //some high number that means the slew rate is effectively instantaneous
+        }
+        
+
+        double currentTime = WPIUtilJNI.now() * 1e-6;
+        double elapsedTime = currentTime - prevTime;
+        double angleDif = SwerveUtils.AngleDifference(inputTranslationDir, currentTranslationDir);
+        if (angleDif < 0.45*Math.PI) {
+            currentTranslationDir = SwerveUtils.StepTowardsCircular(currentTranslationDir, inputTranslationDir, directionSlewRate * elapsedTime);
+            currentTranslationMag = magLimiter.calculate(inputTranslationMag);
+        }
+        else if (angleDif > 0.85*Math.PI) {
+            if (currentTranslationMag > 1e-4) { //some small number to avoid floating-point errors with equality checking
+            // keep currentTranslationDir unchanged
+            currentTranslationMag = magLimiter.calculate(0.0);
+            }
+            else {
+            currentTranslationDir = SwerveUtils.WrapAngle(currentTranslationDir + Math.PI);
+            currentTranslationMag = magLimiter.calculate(inputTranslationMag);
+            }
+        }
+        else {
+            currentTranslationDir = SwerveUtils.StepTowardsCircular(currentTranslationDir, inputTranslationDir, directionSlewRate * elapsedTime);
+            currentTranslationMag = magLimiter.calculate(0.0);
+        }
+        prevTime = currentTime;
+        
+        xSpeedCommanded = currentTranslationMag * Math.cos(currentTranslationDir);
+        ySpeedCommanded = currentTranslationMag * Math.sin(currentTranslationDir);
+        currentRotation = rotLimiter.calculate(rot);
+
+
+        } else {
+        xSpeedCommanded = xSpeed;
+        ySpeedCommanded = ySpeed;
+        currentRotation = rot;
+        }
+
+        // Convert the commanded speeds into the correct units for the drivetrain
+        double xSpeedDelivered = xSpeedCommanded * DriveConstants.kPhysicalMaxSpeedMetersPerSecond;
+        double ySpeedDelivered = ySpeedCommanded * DriveConstants.kPhysicalMaxSpeedMetersPerSecond;
+        double rotDelivered = currentRotation * DriveConstants.kPhysicalMaxAngularSpeedRadiansPerSecond;
+
+        SwerveModuleState[] swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
+            fieldRelative && ControlManager.fieldRelative
+                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered, odometer.getEstimatedPosition().getRotation())
+                : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered));
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+        frontLeft.setDesiredState(swerveModuleStates[0]);
+        frontRight.setDesiredState(swerveModuleStates[1]);
+        backLeft.setDesiredState(swerveModuleStates[2]);
+        backRight.setDesiredState(swerveModuleStates[3]);
     }
 
     /**
